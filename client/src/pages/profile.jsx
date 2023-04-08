422037e7c40b0dacc82b0e523446d6ac3d9b3c5f
pratikpakhale <pratikpakhale20@gmail.com>
2023-04-08 21:05:25 +0530

    readme

---
+++ client/src/pages/profile.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Profile() {
+  return (
+    <>
+      <Head>
+        <title>Profile | Rewind</title>
+        <meta name='description' content='Rewind' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/logo.svg' />
+      </Head>
+    </>
+  )
+}
