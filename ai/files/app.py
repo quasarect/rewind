f3da9b803ed6b278af34ca1d3a00ae65e0dd38da
Chinmayee <chinmayeep2753@gmail.com>
2023-04-16 23:18:15 +0530

    Updated app.py

--- ai/files/app.py
+++ ai/files/app.py
@@ -24,6 +24,7 @@ collection2 = db["keys"]
 
 def generate_tagline():
     user_id = request.headers.get('Authorization').split(' ')[1]
+    #user_id = "6439bfa512b767882aee8b9b"
     # get data from db
     result1 = collection1.find_one({"_id": ObjectId(user_id)})
     spotify_id = result1["spotifyData"]
@@ -90,6 +91,94 @@ def generate_tagline():
     generated_text = response.choices[0].text.strip()
 
     return jsonify({"tagline": generated_text})
+
+
+# voice bot functions
+#take audio file and transcribe into text format. To be edited further
+
+def take_prompt():
+    prompt = "search for enchanted"
+    #prompt = request.json['prompt']
+    return prompt
+
+
+#check if the response is successful
+def check_response(response):
+    if response.status_code == 200:
+        # the request was successful, return the response data
+        return response.json()
+    else:
+        # the request failed, return the error message
+        return {'error': response.text}    
+
+
+#execute the command received from the prompt
+def execute_command(prompt):
+    
+    #get the auth token and user id from idk where.
+        #if have auth token, then use it to create userid or vice versa
+        #auth_token and userid are hardcoded
+    auth = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MzliZmE1MTJiNzY3ODgyYWVlOGI5YiIsInVzZXJuYW1lIjoidXNlciIsIl92IjoiMS4wLjAiLCJpYXQiOjE2ODE2NzMwNDEsImV4cCI6MTY4MjI3Nzg0MX0.Ylu2myZVgDlSwFrsc3h3g2Is49wgeRl4DAYXYCYBL7I"
+    #userid = "6439bfa512b767882aee8b9b"
+    
+    base = os.getenv("BASE_URL")
+    
+    #various prompts to be used
+    create_post = f"the url for creating a post is {base}posts/create and the method is POST and payload consists of only text"
+    search_user = f"the url for searching a user is {base}search/username?username=((name)) and the method is GET"
+    search_song = f"the url for searching a song is {base}search/song?text=((song)) and the method is GET"
+    response_format = '{"url"= "", "method" = " ", "payload" = {"text" = " "}}'
+    
+    #create the prompt for the openai api
+    user_prompt = f'{prompt}. fit the given query only one of the following calls. {search_song}. {create_post}. {search_user}.give the url, method and payload required for making an api call stored. Give all this in a dictionary format.Respond strictly in given format {response_format}.'
+    
+    #provide the prompt to the openai api
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages= [{"role": "system", "content": "hello world"},
+        {"role": "user", "content": user_prompt}],
+        # temperature=0,
+        # max_tokens=100,
+        # top_p=1,
+        # frequency_penalty=0,
+        # presence_penalty=0
+    )
+
+    #receive the response from the openai api and convert it to a dictionary
+    #response will be in string format
+    
+    response = response['choices'][0]['message']['content']
+    response = eval(response)
+    print(response)
+
+    #create the headers for the api call
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {auth}'
+    }
+
+    #get the url, method and payload from the response
+    method = response.get("method")
+    url = response.get("url")
+    payload = response.get("payload")
+
+    #make the api call
+    response = requests.request(method, url, headers=headers, json=payload,timeout=120)
+
+    #check if the response is successful
+    return check_response(response)
+
+
+
+
+# voice bot route
+@app.route('/execute', methods=['GET','POST'])
+def execute():
+    prompt = take_prompt()
+    response = execute_command(prompt)
+    return jsonify(response)
+
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
