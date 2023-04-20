89d3a47194a4a645ccc274d2fac071e6397cd981
pratikpakhale <pratikpakhale20@gmail.com>
2023-04-20 14:34:31 +0530

    try deploy

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,13 +1,13 @@
 import { io } from 'socket.io-client'
 
-export const chatSocket = io(import.meta.env.VITE_SOCKET_ENDPOINT + '/chat', {
+export const chatSocket = io(import.meta.env.VITE_API_ENDPOINT + '/chat', {
   autoConnect: false,
   extraHeaders: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
   },
 })
 
-export const musicSocket = io(import.meta.env.VITE_SOCKET_ENDPOINT + '/music', {
+export const musicSocket = io(import.meta.env.VITE_API_ENDPOINT + '/music', {
   autoConnect: false,
   extraHeaders: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
